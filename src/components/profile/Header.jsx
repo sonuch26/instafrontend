f4367a17cee17ed63db17e99d447acdb08cb83b2
Your Name <you@example.com>
2023-11-10 11:43:59 +0530

    initial commit

--- src/components/profile/Header.jsx
+++ src/components/profile/Header.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
-//import SettingsIcon from '@mui/icons-material/Settings';
+import "../../styles/header.css"
+import SettingsIcon from '@mui/icons-material/Settings';
 const Header = ({myInfo}) => {
     return(
-        <div style={{display:"flex",width:'100%',border:'1px solid grey'}}>
+        <div style={{display:"flex",justifyContent:"center",gap:"10vh",marginTop:"10vh",width:'100%',border:'1px solid grey'}}>
         <img style={{width:'20vh',height:"20vh",borderRadius:"50%",objectFit:"cover"}} src={`${myInfo.profilePic}`} alt=""/>
-        <div style={{display:"flex",gap:"20px"}}>
-            <span>{myInfo.username}</span>
-            <span>Edit Profile</span>
-            <span>View Archive</span>
-            {/*<span>SettingApplicationsIcon</span>*/}
+        <div>
+            <div style={{border:"1px solid grey",display:"flex",gap:"20px"}}>
+            <span style={{fontSize:'20px'}}>{myInfo.username}</span>
+            <span class="profile_header_buttons">Edit Profile</span>
+            <span class="profile_header_buttons">View Archive</span>
+            <span><SettingsIcon/></span>
+            </div>
+            <div style={{display:'flex',justifyContent:'space-around',marginTop:"20px"}}>
+                <span>{myInfo.post} posts</span>
+                <span>{myInfo.followers} followers</span>
+                <span>{myInfo.following} following</span>
+            </div>
+            <div style={{marginTop:"20px",marginLeft:"20px"}}>
+                {myInfo.name}
+            </div>
+            <p style={{marginLeft:"20px"}}>
+                {myInfo.Bio}
+            </p>
+
         </div>
-        </div>
+    </div>
     )
 }
 export default Header
